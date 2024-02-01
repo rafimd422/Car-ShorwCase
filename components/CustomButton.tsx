29703de9232839f74eaa79bcec01161b2e59fd4f
rafimd422 <rafimd2222@gmail.com>
2024-02-01 16:33:16 +0600

    primarily complated the hero section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { CustomButtonProps } from "@/Types"
+import Image from "next/image"
+
+const CustomButton = ({title, containerStyle, handleClick}:CustomButtonProps) => {
+  return (
+    <button disabled={false} type="button" className={`custom-btn ${containerStyle}`} onClick={handleClick}>
+      
+      <span className={`flex-1`}>
+{title}
+      </span>
+
+    </button>
+  )
+}
+
+export default CustomButton
