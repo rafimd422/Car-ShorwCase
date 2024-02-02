7912f9e7a93062fb9e3581849c43529dedad4503
rafimd422 <rafimd2222@gmail.com>
2024-02-02 22:07:52 +0600

    successfully complated the footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,60 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/logo.svg";
+import { footerLinks } from "@/constants";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100 xl:mx-24">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src={logo}
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            Carhub 2024 <br />
+            All right reserved
+          </p>
+        </div>
 
-export default Footer
+        <div className="footer__links">
+          {footerLinks.map((linkGroup, index) => (
+            <div key={index}>
+              <h3 className="font-bold">{linkGroup.title}</h3>
+              <div className="flex flex-col">
+                {linkGroup.links.map((item, subIndex) => (
+                  <Link key={subIndex} className="padding-y" href={item.url}>
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        </div>
+
+        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+                 
+                    
+                 <p>
+                  @2024 CarHub. ALl Rights Reserved
+                 </p>
+                 
+                  <div className="footer__copyrights-link">
+                    <Link href={'/'} className="text-gray-500">
+                      Privacy Policy
+                    </Link>
+                    <Link href={'/'} className="text-gray-500">
+                      Terms of Use
+                    </Link>
+                  </div>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
