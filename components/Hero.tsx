7274bf0fcb8890b52b8fcb8a5af8766ea1249205
rafimd422 <rafimd2222@gmail.com>
2024-02-03 14:45:35 +0600

    updated the overlay positioning of the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import { CustomButton } from "."
-import hero from '@/public/hero.png'
+import Image from "next/image";
+
+import { CustomButton } from ".";
 
 const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-const handleScroll = () => {
-
-}
-
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-            Find,book,or rent a car - quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
-        
-        <p className="hero_subtitle">
-            Streamline your car rental experience with our effortless booking process
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
         </p>
 
-<CustomButton title="Explore Cars" containerStyle="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
+        <CustomButton
+          title="Explore Cars"
+          containerStyle="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src={hero} alt="hero" fill className="object-contain" />
-            <div className="hero__image-overlay">
-            
-            </div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
