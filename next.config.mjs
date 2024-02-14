6d6e6fec224a5ed8187165a42933e819c08fe8b1
rafimd422 <rafimd2222@gmail.com>
2024-02-14 18:50:28 +0600

    updated the next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      domains: ["cdn.imagin.studio"]
+  }
+}
+
+export default nextConfig
