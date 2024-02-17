9051e1e7e56aa3e048b37cdc61adb72bf8eeb59d
rafimd422 <rafimd2222@gmail.com>
2024-02-17 12:39:49 +0600

    fixed the image importation of magnifying glass from the search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,13 +3,14 @@
     import Image from "next/image";
     import React, { useState } from "react";
     import { useRouter } from "next/navigation";
+  import magnifying from '@/public/magnifying-glass.svg'
     
     import SearchManufacturer from "./SearchManufacturer";
     
     const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
       <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
         <Image
-          src={"/magnifying-glass.svg"}
+          src={magnifying}
           alt={"magnifying glass"}
           width={40}
           height={40}
