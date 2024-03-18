d4c03fd5166297e2dd7c6b035dc9810fc8072da5
rafimd422 <rafimd2222@gmail.com>
2024-03-18 16:05:56 +0600

    added ts for fetchCars

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
-import { CarProps } from "@/Types";
+import { CarProps, FilterProps } from "@/Types";
 import axios from "axios";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
     const headers = {
         'X-RapidAPI-Key': '0b5cc00c89mshfba49238494bf14p10722ejsn1bcab7b85a53',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
@@ -37,7 +37,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
-  
+
     url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
